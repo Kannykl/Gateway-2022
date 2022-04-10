68319d7c227fd1246eae824a811ac4e9a1979b11
barabass <serg1995@yandex.ru>
2022-04-10 21:07:15 +0300

    руками поправил кривой merge

--- database_api/tests/user_tests.py
+++ database_api/tests/user_tests.py
@@ -5,13 +5,8 @@ from httpx import AsyncClient
 
 @pytest.mark.asyncio
 async def test_create_valid_user(clear_test_users_db, valid_user_data):
-<<<<<<< HEAD
     async with AsyncClient(app=app, base_url="http://localhost/db/") as ac:
         response = await ac.post("create_user/", json=valid_user_data)
-=======
-    async with AsyncClient(app=app, base_url="http://localhost") as ac:
-        response = await ac.post("/db/create_user/", json=valid_user_data)
->>>>>>> origin/db_tests
     assert response.status_code == 201
     new_user = response.json()
     assert new_user['email'] == "user@example.com"
@@ -20,19 +15,13 @@ async def test_create_valid_user(clear_test_users_db, valid_user_data):
 
 @pytest.mark.asyncio
 async def test_get_users(test_db_with_one_test_user):
-<<<<<<< HEAD
     async with AsyncClient(app=app, base_url="http://localhost/db/") as ac:
         response = await ac.get(f"get_users/?count=1")
-=======
-    async with AsyncClient(app=app, base_url="http://localhost") as ac:
-        response = await ac.get(f"/db/get_users/1")
->>>>>>> origin/db_tests
     assert response.status_code == 200
     assert len(response.json()) == 1
 
 
 @pytest.mark.asyncio
-<<<<<<< HEAD
 async def test_get_user_by_email(test_db_with_one_test_user, user_to_insert):
     async with AsyncClient(app=app, base_url="http://localhost/db/") as ac:
         response = await ac.get(f"get_user_by_email/?email={user_to_insert['email']}")
@@ -71,11 +60,4 @@ async def test_update_user_email(test_db_with_one_test_user, user_to_insert, new
     updated_user = response.json()
     assert response.status_code == 200
     assert updated_user['email'] == new_email_for_user
-=======
-async def test_get_user_by_email(test_db_with_one_test_user):
-    async with AsyncClient(app=app, base_url="http://localhost") as ac:
-        response = await ac.get(f"/db/get_user_by_email/?email=user@example.com")
-    assert response.status_code == 200
-    user = response.json()
-    assert user['email'] == "user@example.com"
->>>>>>> origin/db_tests
+
